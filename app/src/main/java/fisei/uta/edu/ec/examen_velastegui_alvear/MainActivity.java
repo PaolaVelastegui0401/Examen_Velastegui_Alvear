52297bd5883ff1bc6ea3fce725b31f3d5e085376
PaolaVelastegui0401 <paovelastegui@hotmail.com>
2022-05-30 18:25:32 -0500

    Commit 3

--- app/src/main/java/fisei/uta/edu/ec/examen_velastegui_alvear/MainActivity.java
+++ app/src/main/java/fisei/uta/edu/ec/examen_velastegui_alvear/MainActivity.java
@@ -2,13 +2,25 @@ package fisei.uta.edu.ec.examen_velastegui_alvear;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void Seguir_VLAV(View view){
+        Intent siguiente = new Intent(this, MainActivity2_VLAV.class);
+
+        startActivity(siguiente);
+    }
+
 }
