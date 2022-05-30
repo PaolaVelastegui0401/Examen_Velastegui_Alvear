27786839654f6390938a794f7354b593ffb1c7de
Pablo Alvear <alejozx456@gmail.com>
2022-05-30 17:36:28 -0500

    Segunda Pantalla

---
+++ app/src/main/java/fisei/uta/edu/ec/examen_velastegui_alvear/MainActivity2_VLAV.java
@@ -0,0 +1,14 @@
+package fisei.uta.edu.ec.examen_velastegui_alvear;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity2_VLAV extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_activity2_vlav);
+    }
+}
